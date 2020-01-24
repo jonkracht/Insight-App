af93fbcf0e47900fc6924bf904cedff5f91b180c
jonkracht <60161516+jonkracht@users.noreply.github.com>
2020-01-24 15:33:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+# Functions to display dataframes/tables:  st.dataframe(), st.table()
+
+# A variable alone on a line will be printed.
+
+# Charting libraries Matplotlib, Altair, Deck.GI etc.
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
