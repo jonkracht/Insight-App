9c65578d1539acc0eaafe57be89eac40530809d9
jonkracht <60161516+jonkracht@users.noreply.github.com>
2020-01-24 15:23:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st
 st.title('Hello-world')
+
+# st.write():  prints text, data, figures
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
