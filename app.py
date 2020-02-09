227e3e2b4c1df2498b091ad2916237547dcc61d7
Jon Kracht <jonkracht@gmail.com>
2020-02-08 22:17:13 -0500

    Incorporated map

--- app.py
+++ app.py
@@ -55,37 +55,6 @@ def get_latlon_from_zip(zip_code):
     return (result.latitude, result.longitude)
 
 
-# def plot_courses_map(df):
-#     """
-#     Function to plot a map where locations are determined by the input dataframe df.
-#     """
-#
-#     import matplotlib.pyplot as plt
-#     import geopandas as gpd
-#     from shapely.geometry import Point, Polygon
-#
-#     map_file = '/home/jon/PycharmProjects/jon-insight-project/data/external/cb_2018_us_nation_20m/cb_2018_us_nation_20m.shp'
-#
-#     crs = {'init': 'eosg:4326'}
-#
-#     street_map = gpd.read_file(map_file)
-#
-#     geometry = [Point(xy) for xy in zip(df['longitude'], df['latitude'])]
-#
-#     geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
-#
-#     geo_df.head()
-#
-#     fig, ax = plt.subplots(figsize=(15, 15))
-#     street_map.plot(ax=ax, alpha=0.4, color='grey')
-#     geo_df.plot(ax=ax, markersize=20, color='green', marker='o', alpha=0.4)
-#     ax.set_xlabel('Longitude')
-#     ax.set_ylabel('Latitude')
-#     # ax.set_title('Disc golf courses in the US')
-#     plt.show()
-#
-#     return
-
 
 def find_nearby_courses(df, start_zip, max_drive_time):
     """Update dataframe of courses to only those within a certain distance of starting location."""
@@ -133,9 +102,9 @@ def get_user_prefs():
         difficulty_map = {'No preference': 'No preference', 'Easy': 0, 'Moderate': 1, 'Difficult': 2}
 
 
-        prefs['hills'] = hill_map[st.selectbox('Hills:', ['No preference', 'Mostly Flat', 'Moderately Hilly', 'Very Hilly'])]
-        prefs['woods'] = wood_map[st.selectbox('Woods:', ['No preference', 'Lightly Wooded', 'Moderately Wooded', 'Heavily Wooded'])]
-        prefs['difficulty'] = difficulty_map[st.selectbox('Difficulty:', ['No preference', 'Easy', 'Moderate', 'Difficult'])]
+        prefs['hills'] = hill_map[st.sidebar.selectbox('Hills:', ['No preference', 'Mostly Flat', 'Moderately Hilly', 'Very Hilly'])]
+        prefs['woods'] = wood_map[st.sidebar.selectbox('Woods:', ['No preference', 'Lightly Wooded', 'Moderately Wooded', 'Heavily Wooded'])]
+        prefs['difficulty'] = difficulty_map[st.sidebar.selectbox('Difficulty:', ['No preference', 'Easy', 'Moderate', 'Difficult'])]
 
     #prefs['max_length'] = st.text_input("Maximum length course to play:")
     #prefs['max_length'] = st.selectbox("Maximum length course to play:", ['No preference', '3000', '6000', '9000'])
@@ -241,17 +210,52 @@ def main():
                 #st.dataframe(destination[cols_to_display])
 
 
-
-            st.subheader('\nYour LocalRoute:')
+            st.success('\nYour LocalRoute:')
 
             # Create a map of the filtered data)
             #st.write(all_destinations[['latitude', 'longitude']])
 
-            st.map(all_destinations[['latitude', 'longitude']])
+            #st.map(all_destinations[['latitude', 'longitude']])
+
+            plot_df = all_destinations[['latitude', 'longitude']]
 
 
 
-            st.dataframe(all_destinations)
+
+            st.deck_gl_chart(
+                viewport = {
+                    'latitude': plot_df['latitude'].mean(),
+                    'longitude': plot_df['longitude'].mean(),
+                    'zoom': 8,
+                    'angle': 0
+                },
+                layers = [
+                           # {'type': 'ScatterplotLayer',
+                           # 'data':
+                           # 'radiusScale': 500,
+                           # 'radiusMinPixels': 100,
+                           # 'getFillColor': [248, 24, 148],
+                           # 'opacity': 0
+                           # },
+                        {'type': 'ScatterplotLayer',
+                           'data': plot_df,
+                           'radiusScale': 50,
+                           'radiusMinPixels': 5,
+                           'getFillColor': [26, 150, 103],
+                           'opacity': 0.25,
+                           'extruded': True
+                            },
+                            {'type': 'ScatterplotLayer',
+                            'data': df,
+                            'radiusScale': 5,
+                            'radiusMinPixels': 5,
+                            'getFillColor': [255, 0, 0],
+                            'opacity': 0.05,
+                            'extruded': True
+                            }
+                          ])
+
+            st.table(all_destinations[cols_to_display], )
             #st.dataframe(all_destinations[cols_to_display])
 
 
